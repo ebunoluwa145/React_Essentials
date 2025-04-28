c04fbb2e679f5ae8e81ec12df1f3686b3e7cb59a
Ebunoluwa <55211794+ebunoluwa145@users.noreply.github.com>
2025-04-28 09:14:29 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import componentsImg from './assets/components.png'
+import { CORE_CONCEPTS} from './data.js'
+import Header from './components/Header/Header.jsx'
+import CoreConcepts from './components/CoreConcepts.jsx';
+import TabButton from './components/TabButton.jsx';
+import{useState} from 'react';
+import { EXAMPLES } from './data.js';
+
+
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState('components')
+
+  function handleSelect(selectedButton){
+    // componenets, jsx
+    setSelectedTopic(selectedButton);
+    console.log(selectedButton);
+    
+
+}
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts"> 
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcepts
+          title="Components"
+          description="The core UI building"
+          image={componentsImg}
+          />
+          <CoreConcepts
+          title={CORE_CONCEPTS[1].title}
+          description={CORE_CONCEPTS[1].description}
+          image={CORE_CONCEPTS[1].image}
+          />
+          <CoreConcepts {...CORE_CONCEPTS[2]}/>
+          <CoreConcepts {...CORE_CONCEPTS[3]}/>
+        </ul>
+        <h2>Time to get started!</h2>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Componenets</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+
+         <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code} </code>
+          </pre>
+         </div>
+
+        </section>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;

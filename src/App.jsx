f310498edb5cf2779c5ae0f6881a9b589300861b
user <user@users-MacBook-Pro.local>
2025-04-29 12:12:54 +0100

    Inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,28 @@ import { EXAMPLES } from './data.js';
 
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components')
+  const [selectedTopic, setSelectedTopic] = useState('')
 
   function handleSelect(selectedButton){
     // componenets, jsx
     setSelectedTopic(selectedButton);
     console.log(selectedButton);
     
+
+}
+
+let tabContent = <p>Please select an option</p>
+
+if(selectedTopic){
+  tabContent=
+  <div id="tab-content">
+          
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code} </code>
+          </pre>
+         </div>
 
 }
   return (
@@ -50,13 +65,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
 
-         <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic].title}</h3>
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>{EXAMPLES[selectedTopic].code} </code>
-          </pre>
-         </div>
+         {tabContent}
 
         </section>
         
